50438d138fac1c630ea4802fa15257c8ac3523ec
Rianfirmansyah071020 <rian071020@gmail.com>
2023-10-16 09:44:33 +0700

    sidebar perbaikan

--- resources/views/layouts/umumlayouts.blade.php
+++ resources/views/layouts/umumlayouts.blade.php
@@ -8,6 +8,11 @@
     <title>RF</title>
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/sass/main.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@600&family=Poppins:wght@300;500;700&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -19,7 +24,23 @@
         <h5 class="loading">Loading....</h5>
     </div>
 
-    @yield('content')
+    <div class="container-fluid overflow-hidden">
+        <div class="row">
+            <div class="col-lg-2 col-xl-2 col-md-3 d-none d-sm-none d-md-block d-xl-block d-lg-block sidebar">
+                <div class="text-logo">
+                    <h1>RF</h1>
+                </div>
+                <hr>
+            </div>
+            <div class="col-lg-10 col-xl-10 col-md-10 col-12 d-sm-block d-md-block d-xl-block d-lg-block content">
+                @yield('content')
+            </div>
+            <div
+                class="col-lg-2 col-xl-2 col-md-2 d-sm-none d-none d-md-block d-xl-block d-lg-block sidebar-disarankan">
+                a
+            </div>
+        </div>
+    </div>
 
     <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('assets/js/main.js') }}"></script>
