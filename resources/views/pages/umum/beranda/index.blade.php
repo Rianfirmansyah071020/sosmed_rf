49a2ad35662625e4424f648e10d1d2ebadfa8c3e
Rianfirmansyah071020 <rian071020@gmail.com>
2023-10-17 11:38:10 +0700

    content

--- resources/views/pages/umum/beranda/index.blade.php
+++ resources/views/pages/umum/beranda/index.blade.php
@@ -1,24 +1,178 @@
 @extends('layouts.umumlayouts')
 @section('content')
     <section class="body">
-        <div class="row d-flex justify-content-center">
-            <div class="col-2 d-flex justify-content-center align-content-center border-1">
+        <div class="row d-flex justify-content-center gap-2">
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
                 <img src="{{ asset('assets/images/profil/1.jpg') }}" alt="" class="img-status">
             </div>
-            <div class="col-2 d-flex justify-content-center align-content-center border-1">
-                <img src="{{ asset('assets/images/profil/2.jpg') }}" alt="" class="img-status">
-            </div>
-            <div class="col-2 d-flex justify-content-center align-content-center border-1">
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
                 <img src="{{ asset('assets/images/profil/3.jpg') }}" alt="" class="img-status">
             </div>
-            <div class="col-2 d-flex justify-content-center align-content-center border-1">
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
                 <img src="{{ asset('assets/images/profil/4.jpg') }}" alt="" class="img-status">
             </div>
-            <div class="col-2 d-flex justify-content-center align-content-center border-1">
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
                 <img src="{{ asset('assets/images/profil/5.jpg') }}" alt="" class="img-status">
             </div>
-            <div class="col-2 d-flex justify-content-center align-content-center border-1">
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
                 <img src="{{ asset('assets/images/profil/6.jpg') }}" alt="" class="img-status">
+            </div>
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
+                <img src="{{ asset('assets/images/profil/7.jpg') }}" alt="" class="img-status">
+            </div>
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
+                <img src="{{ asset('assets/images/profil/8.jpg') }}" alt="" class="img-status">
+            </div>
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
+                <img src="{{ asset('assets/images/profil/10.jpg') }}" alt="" class="img-status">
+            </div>
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
+                <img src="{{ asset('assets/images/profil/1.jpg') }}" alt="" class="img-status">
+            </div>
+            <div class="col-1 d-flex justify-content-center align-content-center border-1">
+                <img src="{{ asset('assets/images/profil/3.jpg') }}" alt="" class="img-status">
+            </div>
+
+        </div>
+
+        <div class="row d-flex justify-content-center border-0 container-content">
+            <div class="col-9 card p-2 mt-3">
+                <div class="row d-flex justify-content-between mb-3">
+                    <div class="col-6"> <img src="{{ asset('assets/images/profil/1.jpg') }}" alt=""
+                            class="img-profil-content"> rinahanisa <span class="jam-content"> . 1 jam</span>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end align-items-center"><i
+                            class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+                <img src="{{ asset('assets/images/profil/1.jpg') }}" alt="" class="img-card-top"
+                    class="data-content">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="icon-content">
+                            <i class="fa-regular fa-heart icon-content-item"></i>
+                            <i class="fa-regular fa-comment icon-content-item"></i>
+                            <i class="fa-regular fa-paper-plane icon-content-item"></i>
+                        </div>
+                        <div class="icon-simpan">
+                            <i class="fa-regular fa-bookmark icon-content-item"></i>
+                        </div>
+                    </div>
+                    <p class="text-like">100k Suka</p>
+                    <p class="text-deskripsi-content">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, quibusdam...
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-9 card p-2 mt-3">
+                <div class="row d-flex justify-content-between mb-3">
+                    <div class="col-6"> <img src="{{ asset('assets/images/profil/3.jpg') }}" alt=""
+                            class="img-profil-content"> rinahanisa <span class="jam-content"> . 1 jam</span>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end align-items-center"><i
+                            class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+                <img src="{{ asset('assets/images/profil/3.jpg') }}" alt="" class="img-card-top"
+                    class="data-content">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="icon-content">
+                            <i class="fa-regular fa-heart icon-content-item"></i>
+                            <i class="fa-regular fa-comment icon-content-item"></i>
+                            <i class="fa-regular fa-paper-plane icon-content-item"></i>
+                        </div>
+                        <div class="icon-simpan">
+                            <i class="fa-regular fa-bookmark icon-content-item"></i>
+                        </div>
+                    </div>
+                    <p class="text-like">100k Suka</p>
+                    <p class="text-deskripsi-content">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, quibusdam...
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-9 card p-2 mt-3">
+                <div class="row d-flex justify-content-between mb-3">
+                    <div class="col-6"> <img src="{{ asset('assets/images/profil/4.jpg') }}" alt=""
+                            class="img-profil-content"> rinahanisa <span class="jam-content"> . 1 jam</span>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end align-items-center"><i
+                            class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+                <img src="{{ asset('assets/images/profil/4.jpg') }}" alt="" class="img-card-top"
+                    class="data-content">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="icon-content">
+                            <i class="fa-regular fa-heart icon-content-item"></i>
+                            <i class="fa-regular fa-comment icon-content-item"></i>
+                            <i class="fa-regular fa-paper-plane icon-content-item"></i>
+                        </div>
+                        <div class="icon-simpan">
+                            <i class="fa-regular fa-bookmark icon-content-item"></i>
+                        </div>
+                    </div>
+                    <p class="text-like">100k Suka</p>
+                    <p class="text-deskripsi-content">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, quibusdam...
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-9 card p-2 mt-3">
+                <div class="row d-flex justify-content-between mb-3">
+                    <div class="col-6"> <img src="{{ asset('assets/images/profil/12.jpg') }}" alt=""
+                            class="img-profil-content"> rinahanisa <span class="jam-content"> . 1 jam</span>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end align-items-center"><i
+                            class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+                <img src="{{ asset('assets/images/profil/12.jpg') }}" alt="" class="img-card-top"
+                    class="data-content">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="icon-content">
+                            <i class="fa-regular fa-heart icon-content-item"></i>
+                            <i class="fa-regular fa-comment icon-content-item"></i>
+                            <i class="fa-regular fa-paper-plane icon-content-item"></i>
+                        </div>
+                        <div class="icon-simpan">
+                            <i class="fa-regular fa-bookmark icon-content-item"></i>
+                        </div>
+                    </div>
+                    <p class="text-like">100k Suka</p>
+                    <p class="text-deskripsi-content">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, quibusdam...
+                    </p>
+                </div>
+            </div>
+            <div class="col-9 card p-2 mt-3">
+                <div class="row d-flex justify-content-between mb-3">
+                    <div class="col-6"> <img src="{{ asset('assets/images/profil/13.jpg') }}" alt=""
+                            class="img-profil-content"> rinahanisa <span class="jam-content"> . 1 jam</span>
+                    </div>
+                    <div class="col-6 d-flex justify-content-end align-items-center"><i
+                            class="fa-solid fa-ellipsis-vertical"></i></div>
+                </div>
+                <img src="{{ asset('assets/images/profil/13.jpg') }}" alt="" class="img-card-top"
+                    class="data-content">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div class="icon-content">
+                            <i class="fa-regular fa-heart icon-content-item"></i>
+                            <i class="fa-regular fa-comment icon-content-item"></i>
+                            <i class="fa-regular fa-paper-plane icon-content-item"></i>
+                        </div>
+                        <div class="icon-simpan">
+                            <i class="fa-regular fa-bookmark icon-content-item"></i>
+                        </div>
+                    </div>
+                    <p class="text-like">100k Suka</p>
+                    <p class="text-deskripsi-content">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, quibusdam...
+                    </p>
+                </div>
             </div>
         </div>
     </section>
